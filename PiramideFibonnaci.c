550d039827ec854b013afa4696f15e8f0682e020
João Guilherme <87271192+JaaumG@users.noreply.github.com>
2022-06-21 13:32:50 -0300

    Create PiramideFibonnaci.c
    Code to generate this pattern: 
    1
    4 6
    9 11 13
    17 19 21 23

---
+++ PiramideFibonnaci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main() {
+	
+	unsigned long num, numA = 2, numB = 1, linhas, i = 0, n=0;
+	unsigned long ini, ini2;
+	printf("Entre com o primeiro valor da piramide \n");
+	scanf("%d", &ini);
+	printf("Entre com a quantidade de linhas \n");
+	scanf("%d", &linhas);
+	linhas--;
+	printf("\n\n%d\n", ini);
+	while (i<linhas){
+		n=0;
+		num = numA+numB;
+		numB = numA;
+		numA = num;
+		ini += numA;
+		printf("%d ", ini);
+		if (i == 0 ){
+			ini2 = ini;
+			ini2 +=2;
+			printf ("%d", ini2);	
+		}
+		for (ini2=ini;n<=i && i!=0; n++){
+			ini2 +=2;
+			printf ("%d ", ini2);
+		}
+		printf("\n");
+		i++;
+		}
+	}
